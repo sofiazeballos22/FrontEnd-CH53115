07b2d5a8a45433a90655c86817f217373f2da5ec
ana sofia zeballos <azeballos512@gmail.com>
2024-08-17 13:42:12 -0300

    Renombrando App.jsx con mayúscula

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,8 @@
 
 // src/App.jsx
 import React from 'react';
+import { BrowserRouter as BrowserRouter } from 'react-router-dom';
+
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 //import Login from './components/Login';
 //import Products from './components/Products';
